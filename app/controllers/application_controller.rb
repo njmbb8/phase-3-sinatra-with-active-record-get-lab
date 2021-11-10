76922d8158f1664d2476fbde7bee108dd1b0309d
njmbb8 <nmcgr001@gmail.com>
2021-11-09 20:58:53 -0500

    Completed Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, :json
 
+  get '/bakeries' do
+    Bakery.all.to_json
+  end
+
+  get '/bakeries/:id' do
+    Bakery.find(params[:id]).to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    BakedGood.order(price: :desc).to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    BakedGood.order(:price).last.to_json
+  end
 end
